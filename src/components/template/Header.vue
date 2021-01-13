edba832603d7e11cb896fa367e31d873890168a9
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-13 20:11:53 -0300

    initial template structure

---
+++ src/components/template/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+  .header {
+    grid-area: header;
+    background: linear-gradient(to right, #1e4694, #49a7c1);
+    padding: 20px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
