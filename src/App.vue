08f589624191f8937ef9b123175bdddec048095f
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-14 11:40:05 -0300

    user menu component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<div id="app" :class="{'hide-menu': !isMenuVisible}">
-		<Header title="Cod3r - Base de conhecimento" :hideToggle="false" />
+		<Header title="Cod3r - Base de conhecimento"
+			:hideToggle="false"
+			:hideUserDropdown="false" />
 		<Menu />
 		<Content />
 		<Footer />
