5ee8bf356f19c8f533569ed209f2c11804bb52b6
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-18 10:47:35 -0300

    authentication: alter store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-	<div id="app" :class="{'hide-menu': !isMenuVisible}">
+	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
 		<Header title="Cod3r - Base de conhecimento"
-			:hideToggle="false"
-			:hideUserDropdown="false" />
-		<Menu />
+			:hideToggle="!user"
+			:hideUserDropdown="!user" />
+		<Menu v-if="user" />
 		<Content />
 		<Footer />
 	</div>
@@ -19,7 +19,7 @@ import Footer from '@/components/template/Footer'
 export default {
 	name: "App",
 	components: { Header, Menu, Content, Footer },
-	computed: mapState(['isMenuVisible'])
+	computed: mapState(['isMenuVisible', 'user'])
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -10,9 +10,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// TEMPORARIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkV6ZXF1aWFzIFJvY2hhIiwiZW1haWwiOiJlemVxdWlhcy5yb2NoYTlAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxMDY1OTQ5NiwiZXhwIjoxNjEwOTE4Njk2fQ.5IyCKDbaD28Z6AnsKYlpJLRXV9jEzHXtMZFpNtcdzho'
-
 new Vue({
   store,
   router,
